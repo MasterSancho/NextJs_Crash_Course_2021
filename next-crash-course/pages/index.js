42e8df3c079ed240554fad68bdf2fbf8f6a4d252
Alex M <1maltsevalex@gmail.com>
2021-07-28 15:09:01 +0300

    001 -> Stage 1

---
+++ next-crash-course/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+ return (
+  <div>
+   <Head>
+    <title>WebDev newz</title>
+    <meta name='keywords' content='web development, programming' />
+   </Head>
+   Welcome to Next
+  </div>
+ );
+}
